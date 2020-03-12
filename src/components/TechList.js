0237a2a0bb06b4b22fbe953a31e140388bd8ed73
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-03-12 19:48:19 -0300

    add component didMount e component DidUpdate

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,12 +5,32 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece na tela
+
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado smepre que houver alterações nas props ou estado
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  
+  componentWillUnmount() {
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -23,6 +43,7 @@ class TechList extends Component {
       techs: [...this.state.techs, this.state.newTech], 
       newTech: ''
     });
+    
   }
 
   handleDelete = (tech) => {
